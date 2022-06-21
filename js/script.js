87d02c33168d54e89a84ef2edf01403b80777156
Mrfortwash <jeremyharris1996@gmail.com>
2022-06-21 11:40:32 -0400

    2nd change from JS to jquery
    2nd change from JS to jquery

--- js/script.js
+++ js/script.js
@@ -29,13 +29,19 @@
    $('#list').sortable();
 }
 */
-
 let li = $('<li></li>');
 let inputValue = $('#input').val();
 li.append(inputValue);
-
 if (inputValue === '') {
     alert('You must write something!');
 } else {
     $('#list').append(li);
 }
+
+function crossOut() {
+    li.toggleClass('strike')
+}
+
+li.on('dbclick', function (crossOut) {
+    li.toggleClass('strike');
+});
