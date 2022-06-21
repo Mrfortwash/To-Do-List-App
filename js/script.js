ae15855e7f6fff07bd3241e37b932e00dc215884
Mrfortwash <jeremyharris1996@gmail.com>
2022-06-21 12:12:48 -0400

    lint errors in project

--- js/script.js
+++ js/script.js
@@ -1,30 +1,42 @@
+//1. Adding a new item to the list of items: 
 function newItem() {
 
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
+
+
     if (inputValue === '') {
         alert('You must write something!');
     } else {
-        $('#list').append(li);
-        $('#input').val('');
+        $("#list").append(li);
     }
-    //strike out item on list
+    //2. Crossing out an item from the list of items:
+
     function crossOut() {
-        li.toggleClass('strike')
+        li.toggleClass("strike");
     }
-    li.on('dbclick', function crossOut() {
-        li.toggleClass('strike');
+
+    li.on("dblclick", function crossOut() {
+        li.toggleClass("strike");
     });
-    //add delete button to list items
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append('X');
+
+    //3(i). Adding the delete button "X": 
+
+    let crossOutButton = $("<crossOutButton></crossOutButton>");
+    crossOutButton.append(document.createTextNode("X"));
     li.append(crossOutButton);
-    //delete item from list
+
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+
     crossOutButton.on('click', deleteListItem);
     function deleteListItem() {
         li.addClass('delete')
     }
-    //make list sortable
+
+
+    // 4. Reordering the items: 
     $('#list').sortable();
+
+
 }
