d4096687fc0edc9620a91b3eac5edcb6def7bafa
Mrfortwash <jeremyharris1996@gmail.com>
2022-06-21 11:47:40 -0400

    converted last section of JS to jquery
    converted last section of JS to jquery

--- js/script.js
+++ js/script.js
@@ -29,19 +29,40 @@
    $('#list').sortable();
 }
 */
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
-if (inputValue === '') {
-    alert('You must write something!');
-} else {
-    $('#list').append(li);
-}
 
-function crossOut() {
-    li.toggleClass('strike')
-}
+//add new item to list
+function newItem() {
 
-li.on('dbclick', function (crossOut) {
-    li.toggleClass('strike');
-});
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append(li);
+        $('#input').val('');
+    }
+
+    //strike out item on list
+    function crossOut() {
+        li.toggleClass('strike')
+    }
+
+    li.on('dbclick', function crossOut() {
+        li.toggleClass('strike');
+    });
+
+    //add delete button to list items
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append('X');
+    li.append(crossOutButton);
+
+    //delete item from list
+    crossOutButton.on('click', deleteListItem);
+    function deleteListItem() {
+        li.addClass('delete')
+    }
+
+    //make list sortable
+    $('#list').sortable();
+}
